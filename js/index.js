69b4e86bf9f3b124f9c101814174128a5d82c080
Lokendra Puri <lpuri1979@gmail.com>
2022-05-02 15:13:45 +0100

    add navigator's name

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 // Iteration 1: Names and Input
 let haker1 = "James";
 console.log(`The driver's name is ${haker1}`);
+
+let navigator = "David";
+console.log(`The navigaor's name is ${navigator}`);
 //
 // Iteration 2: Conditionals
 
